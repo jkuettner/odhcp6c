559d39e5e42ea3bd39ae3561def44b1ee7537b22
Jerome Küttner <12213711+jkuettner@users.noreply.github.com>
2024-10-10 02:02:50 +0200

    fix misleading-indentation

--- src/dhcpv6.c
+++ src/dhcpv6.c
@@ -1148,9 +1148,11 @@ static int dhcpv6_handle_reply(enum dhcpv6_msg orig, _unused const int rc,
 
 						code = ((int)sdata[0]) << 8 | ((int)sdata[1]);
 
-						if (code == DHCPV6_Success)
+						if (code == DHCPV6_Success) {
 							dhcpv6_successful_once = true;
 							continue;
+						}
+
 
 						dhcpv6_handle_ia_status_code(orig, ia_hdr,
 							code, mdata, mlen, handled_status_codes, &ret);
